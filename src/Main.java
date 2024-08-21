be86f36642ff8b9f5ab7698db976d023a2c38c93
klasmer <klasmer495@gmail.com>
2024-08-21 18:38:45 +0300

    Урок 1 Массивы. Задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //Объявите три массива:
@@ -15,14 +17,20 @@ public class Main {
 
         //Распечатайте на отдельной строчке элементы каждого массива по порядку через запятую. В конце строки запятую ставить не надо.
         System.out.println("Задача 2");
-        System.out.println(arr[0] + " , " + arr[1]+" , " + arr[2]);
-        System.out.println(fractions[0]+" , " + fractions[1]+" , " + fractions[2]);
+        System.out.println(arr[0] + " , " + arr[1] + " , " + arr[2]);
+        System.out.println(fractions[0] + " , " + fractions[1] + " , " + fractions[2]);
         System.out.println(apples[0] + " , " + apples[1]);
 
         //Распечатайте на отдельной строчке элементы каждого массива в обратном порядке через запятую. В конце строки запятую ставить не надо.
         System.out.println("Задача 3");
-        System.out.println(arr[2] + " , " + arr[1]+" , " + arr[0]);
-        System.out.println(fractions[2]+" , " + fractions[1]+" , " + fractions[0]);
+        System.out.println(arr[2] + " , " + arr[1] + " , " + arr[0]);
+        System.out.println(fractions[2] + " , " + fractions[1] + " , " + fractions[0]);
         System.out.println(apples[1] + " , " + apples[0]);
+
+        //Пройдитесь по первому целочисленному массиву и все нечетные числа в нем сделайте четными (нужно прибавить 1).
+        System.out.println("Задача 4");
+        arr[0] = 1 + 1;
+        arr[2] = 3 + 1;
+        System.out.println(Arrays.toString(arr));
     }
 }
