03d701d31ee01382f4c7abcf16d3b19a372baa72
klasmer <klasmer495@gmail.com>
2024-08-21 18:21:53 +0300

    Урок 1 Массивы. Задача 1,2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Объявите три массива:
+        //Целочисленный массив, заполненный тремя цифрами — 1, 2 и 3 — с помощью ключевого слова
+        //new.
+        System.out.println("Задача 1");
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        double[] fractions = {1.57, 7.654, 9.986};
+        int[] apples = new int[2];
+        apples[0] = 2;
+        apples[1] = 3;
+
+        //Распечатайте на отдельной строчке элементы каждого массива по порядку через запятую. В конце строки запятую ставить не надо.
+        System.out.println("Задача 2");
+        System.out.println(arr[0] + " , " + arr[1]+" , " + arr[2]);
+        System.out.println(fractions[0]+" , " + fractions[1]+" , " + fractions[2]);
+        System.out.println(apples[0] + " , " + apples[1]);
     }
 }
